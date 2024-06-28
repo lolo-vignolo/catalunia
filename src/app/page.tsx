9a88bbdd8cc9dc5f2a3fb59238535c95e819259b
lolo-vignolo <vignolo_3@hotmail.com>
2024-06-28 20:21:59 +0200

    changes in confit y modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,30 +10,31 @@ export default function Home() {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        {showModal && <Modal setShowModal={setShowModal} />}
-        <div className={styles.headerContainer}>
-          <Image
-            src="/images/caballo.png"
-            alt="Prades"
-            width={300}
-            height={300}
-          />
-          <h1 className={styles.title}>
-            Auca de la creacio del comtat de les Muntanyes de Prades
-          </h1>
+    <Modal setShowModal={setShowModal} showModal={showModal}>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <div className={styles.headerContainer}>
+            <Image
+              src="/images/caballo.png"
+              alt="Prades"
+              width={300}
+              height={300}
+            />
+            <h1 className={styles.title}>
+              Auca de la creacio del comtat de les Muntanyes de Prades
+            </h1>
+          </div>
+          <div className={styles.cardsContainer}>
+            {cardsData.map((card) => {
+              return (
+                <Suspense key={card.id} fallback={<div>Loading...</div>}>
+                  <Card card={card} setShowModal={setShowModal} />
+                </Suspense>
+              );
+            })}
+          </div>
         </div>
-        <div className={styles.cardsContainer}>
-          {cardsData.map((card) => {
-            return (
-              <Suspense key={card.id} fallback={<div>Loading...</div>}>
-                <Card card={card} setShowModal={setShowModal} />
-              </Suspense>
-            );
-          })}
-        </div>
-      </div>
-    </main>
+      </main>
+    </Modal>
   );
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,9 +4,11 @@ import Confetti from "react-confetti";
 
 interface ModalProps {
   setShowModal: (showModal: boolean) => void;
+  children: React.ReactNode;
+  showModal: boolean;
 }
 
-export const Modal = ({ setShowModal }: ModalProps) => {
+export const Modal = ({ setShowModal, showModal, children }: ModalProps) => {
   const [amount, setAmount] = useState<number>(0);
 
   // contar cuantas cartas se han escaneado
@@ -19,36 +21,42 @@ export const Modal = ({ setShowModal }: ModalProps) => {
   }, []); // Empty dependency array to run only once after initial render
 
   return (
-    <div className={styles.modal}>
-      <Confetti
-        width={1920}
-        height={1080}
-        numberOfPieces={200}
-        friction={0.99}
-        wind={0}
-        gravity={0.1}
-        initialVelocityX={4}
-        initialVelocityY={10}
-        colors={["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]}
-        opacity={1}
-        recycle={true}
-        run={true}
-      />
-      <div className={styles.modalContent}>
-        <h2>¡Felicidades!</h2>
-        <p>Has conseguido un nuevo token</p>
-        <p>Tienes {amount} tokens de un total de 40!</p>
-        <div>
-          <button
-            className={styles.closeButton}
-            onClick={() => {
-              setShowModal(false);
-            }}
-          >
-            close
-          </button>
+    <>
+      <div
+        className={styles.modal}
+        style={{ display: showModal ? "flex" : "none" }}
+      >
+        <Confetti
+          width={1920}
+          height={1080}
+          numberOfPieces={200}
+          friction={0.99}
+          wind={0}
+          gravity={0.1}
+          initialVelocityX={4}
+          initialVelocityY={10}
+          colors={["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]}
+          opacity={1}
+          recycle={true}
+          run={true}
+        />
+        <div className={styles.modalContent}>
+          <h2>¡Felicidades!</h2>
+          <p>Has conseguido un nuevo token</p>
+          <p>Tienes {amount} tokens de un total de 40!</p>
+          <div>
+            <button
+              className={styles.closeButton}
+              onClick={() => {
+                setShowModal(false);
+              }}
+            >
+              close
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      {children}
+    </>
   );
 };
