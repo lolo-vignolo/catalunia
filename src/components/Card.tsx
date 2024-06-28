01baedc05bdff4edfd03a7a059c9b658ae75c32c
lolo-vignolo <vignolo_3@hotmail.com>
2024-06-28 15:59:29 +0200

    first commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,72 @@
+"use client";
+import Image from "next/image";
+import styles from "./card.module.css";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+interface CardProps {
+  card: {
+    id: number;
+    title: string;
+    image: string;
+    description: string;
+    passwordImg: string;
+    passwordDescription: string;
+  };
+}
+
+export const Card = ({ card }: CardProps) => {
+  const searchParams = useSearchParams();
+  const [allowedImg, setAllowedImg] = useState<boolean>(false);
+  const [allowedDescription, setAllowedDescription] = useState<boolean>(false);
+  const allowedIds = ["1234", "5678"];
+
+  const { passwordDescription, passwordImg } = card;
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      const idParam = searchParams.get("id");
+      if (idParam) {
+        const checkIfIdExists = localStorage.getItem("qr-list");
+        if (checkIfIdExists) {
+          const qrList = checkIfIdExists.split(",");
+
+          if (!qrList.includes(idParam)) {
+            localStorage.setItem("qr-list", `${checkIfIdExists},${idParam}`);
+          }
+        }
+      }
+
+      const storedQrList = localStorage.getItem("qr-list");
+      if (storedQrList) {
+        const qrList = storedQrList.split(",");
+
+        if (qrList.includes(passwordImg)) {
+          setAllowedImg(true);
+        }
+        if (qrList.includes(passwordDescription)) {
+          setAllowedDescription(true);
+        }
+      }
+    }
+  }, [searchParams]);
+
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.imageContainer}>
+        <Image
+          src={card.image}
+          alt="Prades"
+          style={{ filter: !allowedImg ? "blur(5px)" : "" }}
+          layout="fill"
+        />
+      </div>
+      <div
+        className={styles.bodyContainer}
+        style={{ filter: !allowedDescription ? "blur(5px)" : "" }}
+      >
+        <p className={styles.cardBody}>{card.description}</p>
+      </div>
+    </div>
+  );
+};
