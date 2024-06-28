5ca24f25d250c34eba718f7037c7e88660d988f4
lolo-vignolo <vignolo_3@hotmail.com>
2024-06-28 15:59:29 +0200

    first commit

---
+++ src/data/cardData.ts
@@ -0,0 +1,182 @@
+export const cardsData = [
+  {
+    id: 1,
+    title: "card 1",
+    image: "/images/cards/card-1.png",
+    description:
+      "Elbonreigaumell ha estat moltespavilat enferneiner uncomtatper teniraratllaelsbarons",
+    passwordImg: "1234",
+    passwordDescription: "5678",
+  },
+  {
+    id: 2,
+    title: "card 2",
+    image: "/images/cards/card-2.png",
+    description:
+      "Ambpompaisolemnitat Esfundatelnoucomtat. L'infantRamon Berenguer n'Esfetelcomteprimer",
+    passwordImg: "2345",
+    passwordDescription: "6789",
+  },
+  {
+    id: 3,
+    title: "card 3",
+    image: "/images/cards/card-3.png",
+    description:
+      "Elmenutdedeugermans enrerenoespot quedar; repunfeuihadeprestar juramentdebocaimans",
+    passwordImg: "3456",
+    passwordDescription: "7890",
+  },
+  {
+    id: 4,
+    title: "card 4",
+    image: "/images/cards/card-4.png",
+    description:
+      "Nobles,infantsiprelats, cavallersiciutadans, delspetitsfinsalsmesgrans,al'acte son convidats.",
+    passwordImg: "4567",
+    passwordDescription: "8901",
+  },
+  {
+    id: 5,
+    title: "card 5",
+    image: "/images/cards/card-5.png",
+    description:
+      "Daltdelcinglebenufana lardidaibellaSiurana, antic cap de valiat, fa decor del noucomtat.",
+    passwordImg: "5678",
+    passwordDescription: "9012",
+  },
+  {
+    id: 6,
+    title: "card 6",
+    image: "/images/cards/card-6.png",
+    description:
+      "Prades,lavilacomal n'esdeve la copital; pero comquehifamoltfred sovint viuen a Falset.",
+    passwordImg: "6789",
+    passwordDescription: "0123",
+  },
+  {
+    id: 7,
+    title: "card 7",
+    image: "/images/cards/card-7.png",
+    description:
+      "ElMontsantilaGritella sabracenaCornudella Delcomtatvilapregona passaaser-nelasegona",
+    passwordImg: "7890",
+    passwordDescription: "1234",
+  },
+  {
+    id: 8,
+    title: "card 8",
+    image: "/images/cards/card-8.png",
+    description:
+      "Hihaalgunslocs onel senyor comparteixelseupoder: laCartoixoamb elprior, certesvilesambPoblet",
+    passwordImg: "8901",
+    passwordDescription: "2345",
+  },
+  {
+    id: 9,
+    title: "card 9",
+    image: "/images/cards/card-9.png",
+    description:
+      "SonlesMuntanyesdePrades l'escenariontotcomenca. is'hiajuntenvincutades lesterresdelnobleEntenca",
+    passwordImg: "9012",
+    passwordDescription: "3456",
+  },
+  {
+    id: 10,
+    title: "card 10",
+    image: "/images/cards/card-10.png",
+    description:
+      "Dela seva mort enca, tornarenalaCorona Falset,Altafallaora ProtdinTiwiuoMorca",
+    passwordImg: "0123",
+    passwordDescription: "4567",
+  },
+  {
+    id: 11,
+    title: "card 11",
+    image: "/images/cards/card-11.png",
+    description:
+      "lencaras'hiafegiria eldominideGarcia Aixiesfundaunsenvoriu entre serres,maririu.",
+    passwordImg: "1234",
+    passwordDescription: "5678",
+  },
+  {
+    id: 12,
+    title: "card 12",
+    image: "/images/cards/card-12.png",
+    description:
+      "Aquestadonacib fixa unacondicio: elcomte hoconservara semprequeunhereuhihaura",
+    passwordImg: "2345",
+    passwordDescription: "6789",
+  },
+  {
+    id: 13,
+    title: "card 13",
+    image: "/images/cards/card-13.png",
+    description:
+      "Siels comtes no tenen fill, legitimimasculi, talcomdiuelpergami el dominiestaenperill.",
+    passwordImg: "3456",
+    passwordDescription: "7890",
+  },
+  {
+    id: 14,
+    title: "card 14",
+    image: "/images/cards/card-14.png",
+    description:
+      "Aixi ho estableix la lei quehadictat elmateixrei tantcomtatcom baronia delmonarca tot seria.",
+    passwordImg: "4567",
+    passwordDescription: "8901",
+  },
+  {
+    id: 15,
+    title: "card 15",
+    image: "/images/cards/card-15.png",
+    description:
+      "Lariquesa delcomtat creix decop bencap amunt, quandeixad'estar amagat totelmetallde Bellmunt",
+    passwordImg: "5678",
+    passwordDescription: "9012",
+  },
+  {
+    id: 16,
+    title: "card 16",
+    image: "/images/cards/card-16.png",
+    description:
+      "Terresfertilsiramats, boscos,firesimercats sonrecursosdestacats d'unsocspriilegiats.",
+    passwordImg: "6789",
+    passwordDescription: "0123",
+  },
+  {
+    id: 17,
+    title: "card 17",
+    image: "/images/cards/card-17.png",
+    description:
+      "Pero no totrutllabe desde lomalanyprimer ; convindraempenyorar la RibailAleixar.",
+    passwordImg: "7890",
+    passwordDescription: "1234",
+  },
+  {
+    id: 18,
+    title: "card 18",
+    image: "/images/cards/card-18.png",
+    description:
+      "No hihatrampesnienganys, us ho dic de veritat: delnaixement delcomtat, se'ncelebrenset-cents anys",
+    passwordImg: "8901",
+    passwordDescription: "2345",
+  },
+  {
+    id: 19,
+    title: "card 19",
+    image: "/images/cards/card-19.png",
+    description:
+      "Siuspreguntenperladata no dubteuicomjo faig contesteu:el6demaig demiltres-centsvint-i-quatre",
+    passwordImg: "9012",
+    passwordDescription: "3456",
+  },
+  {
+    id: 20,
+    title: "card 20",
+    image: "/images/cards/card-20.png",
+    description:
+      "I de tota aquesta historia perguardar-nelamemoria se'nfaunampliredactat ben signatisegellat.",
+    passwordImg: "0123",
+    passwordDescription: "4567",
+  },
+];
