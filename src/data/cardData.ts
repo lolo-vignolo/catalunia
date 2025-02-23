5ec700915e05c351801b1ef6867b3a788ee344f6
LVignoloCellectEnergy <l.vignolo@cellectenergy.com>
2025-02-23 15:12:00 +0100

    pergamino change

--- src/data/cardData.ts
+++ src/data/cardData.ts
@@ -44,7 +44,7 @@ export const cardsData = [
   },
   {
     id: 5,
-    titles: "La Febró",
+    titles: ["La Febró", "El Molar"],
     portadaImg: "/images/cardsBlackAndWhite/cat-bn-5.png",
     image: "/images/cards/card-5.png",
     description:
@@ -52,28 +52,11 @@ export const cardsData = [
     passwordImg: "84736291",
     passwordDescription: "56473820",
   },
-  {
-    id: 5,
-    titles: "El Molar",
-    portadaImg: "/images/cardsBlackAndWhite/cat-bn-5.png",
-    image: "/images/cards/card-5.png",
-    description:
-      "Daltdelcinglebenufana lardidaibellaSiurana, antic cap de valiat, fa decor del noucomtat.",
-    passwordImg: "84736291",
-    passwordDescription: "56473820",
-  },
+
   {
     id: 6,
-    titles: "Arbolí",
-    image: "/images/cards/card-6.png",
-    description:
-      "Prades,lavilacomal n'esdeve la copital; pero comquehifamoltfred sovint viuen a Falset.",
-    passwordImg: "28374651",
-    passwordDescription: "73829106",
-  },
-  {
-    id: 6,
-    titles: "El Masroig",
+    titles: ["Arbolí", "El Masroig"],
+    portadaImg: "/images/cardsBlackAndWhite/cat-bn-6.png",
     image: "/images/cards/card-6.png",
     description:
       "Prades,lavilacomal n'esdeve la copital; pero comquehifamoltfred sovint viuen a Falset.",
@@ -82,16 +65,8 @@ export const cardsData = [
   },
   {
     id: 7,
-    titles: "Cornudella de Montsant",
-    image: "/images/cards/card-7.png",
-    description:
-      "ElMontsantilaGritella sabracenaCornudella Delcomtatvilapregona passaaser-nelasegona",
-    passwordImg: "91028374",
-    passwordDescription: "63719285",
-  },
-  {
-    id: 7,
-    titles: "Marçà",
+    titles: ["Cornudella de Montsant", "Marçà"],
+    portadaImg: "/images/cardsBlackAndWhite/cat-bn-7.png",
     image: "/images/cards/card-7.png",
     description:
       "ElMontsantilaGritella sabracenaCornudella Delcomtatvilapregona passaaser-nelasegona",
@@ -100,16 +75,8 @@ export const cardsData = [
   },
   {
     id: 8,
-    titles: "Albarca",
-    image: "/images/cards/card-8.png",
-    description:
-      "Hihaalgunslocs onel senyor comparteixelseupoder: laCartoixoamb elprior, certesvilesambPoblet",
-    passwordImg: "74839201",
-    passwordDescription: "28374659",
-  },
-  {
-    id: 8,
-    titles: "Capçanes",
+    titles: ["Albarca", "Capçanes"],
+    portadaImg: "/images/cardsBlackAndWhite/cat-bn-8.png",
     image: "/images/cards/card-8.png",
     description:
       "Hihaalgunslocs onel senyor comparteixelseupoder: laCartoixoamb elprior, certesvilesambPoblet",
@@ -118,34 +85,19 @@ export const cardsData = [
   },
   {
     id: 9,
-    titles: "Ulldemolins",
+    titles: ["Ulldemolins", "Els Guiamets"],
+    portadaImg: "/images/cardsBlackAndWhite/cat-bn-9.png",
     image: "/images/cards/card-9.png",
     description:
       "SonlesMuntanyesdePrades l'escenariontotcomenca. is'hiajuntenvincutades lesterresdelnobleEntenca",
     passwordImg: "91029384",
     passwordDescription: "47583920",
   },
-  {
-    id: 9,
-    titles: "Els Guiamets",
-    image: "/images/cards/card-9.png",
-    description:
-      "SonlesMuntanyesdePrades l'escenariontotcomenca. is'hiajuntenvincutades lesterresdelnobleEntenca",
-    passwordImg: "91029384",
-    passwordDescription: "47583920",
-  },
+
   {
     id: 10,
-    titles: "Vilanova de Prades",
-    image: "/images/cards/card-10.png",
-    description:
-      "Dela seva mort enca, tornarenalaCorona Falset,Altafallaora ProtdinTiwiuoMorca",
-    passwordImg: "74829301",
-    passwordDescription: "38271649",
-  },
-  {
-    id: 10,
-    titles: "Garcia",
+    titles: ["Vilanova de Prades", "Garcia"],
+    portadaImg: "/images/cardsBlackAndWhite/cat-bn-10.png",
     image: "/images/cards/card-10.png",
     description:
       "Dela seva mort enca, tornarenalaCorona Falset,Altafallaora ProtdinTiwiuoMorca",
@@ -154,16 +106,7 @@ export const cardsData = [
   },
   {
     id: 11,
-    titles: "Vallclara",
-    image: "/images/cards/card-11.png",
-    description:
-      "lencaras'hiafegiria eldominideGarcia Aixiesfundaunsenvoriu entre serres,maririu.",
-    passwordImg: "57382910",
-    passwordDescription: "94857362",
-  },
-  {
-    id: 11,
-    titles: "Móra d´Ebre",
+    titles: ["Vallclara", "Móra d´Ebre"],
     image: "/images/cards/card-11.png",
     description:
       "lencaras'hiafegiria eldominideGarcia Aixiesfundaunsenvoriu entre serres,maririu.",
@@ -172,16 +115,8 @@ export const cardsData = [
   },
   {
     id: 12,
-    titles: "La Riba",
-    image: "/images/cards/card-12.png",
-    description:
-      "Aquestadonacib fixa unacondicio: elcomte hoconservara semprequeunhereuhihaura",
-    passwordImg: "38475619",
-    passwordDescription: "64718293",
-  },
-  {
-    id: 12,
-    titles: "Tivissa",
+    titles: ["La Riba", "Tivissa"],
+    portadaImg: "/images/cardsBlackAndWhite/cat-bn-12.png",
     image: "/images/cards/card-12.png",
     description:
       "Aquestadonacib fixa unacondicio: elcomte hoconservara semprequeunhereuhihaura",
@@ -190,16 +125,8 @@ export const cardsData = [
   },
   {
     id: 13,
-    titles: "Farena",
-    image: "/images/cards/card-13.png",
-    description:
-      "Siels comtes no tenen fill, legitimimasculi, talcomdiuelpergami el dominiestaenperill.",
-    passwordImg: "82937456",
-    passwordDescription: "46573920",
-  },
-  {
-    id: 13,
-    titles: "Pratdip",
+    titles: ["Farena", "Pratdip"],
+    portadaImg: "/images/cardsBlackAndWhite/cat-bn-13.png",
     image: "/images/cards/card-13.png",
     description:
       "Siels comtes no tenen fill, legitimimasculi, talcomdiuelpergami el dominiestaenperill.",
@@ -208,16 +135,8 @@ export const cardsData = [
   },
   {
     id: 14,
-    titles: "Mont-ral",
-    image: "/images/cards/card-14.png",
-    description:
-      "Aixi ho estableix la lei quehadictat elmateixrei tantcomtatcom baronia delmonarca tot seria.",
-    passwordImg: "18293746",
-    passwordDescription: "37482910",
-  },
-  {
-    id: 14,
-    titles: "Vandellòs",
+    titles: ["Mont-ral", "Vandellòs"],
+    portadaImg: "/images/cardsBlackAndWhite/cat-bn-14.png",
     image: "/images/cards/card-14.png",
     description:
       "Aixi ho estableix la lei quehadictat elmateixrei tantcomtatcom baronia delmonarca tot seria.",
@@ -226,16 +145,8 @@ export const cardsData = [
   },
   {
     id: 15,
-    titles: "L´Aleixar",
-    image: "/images/cards/card-15.png",
-    description:
-      "Lariquesa delcomtat creix decop bencap amunt, quandeixad'estar amagat totelmetallde Bellmunt",
-    passwordImg: "48573629",
-    passwordDescription: "28394057",
-  },
-  {
-    id: 15,
-    titles: "L´Hospitalet de l´Infant",
+    titles: ["L´Aleixar", "L´Hospitalet de l´Infant"],
+    portadaImg: "/images/cardsBlackAndWhite/cat-bn-15.png",
     image: "/images/cards/card-15.png",
     description:
       "Lariquesa delcomtat creix decop bencap amunt, quandeixad'estar amagat totelmetallde Bellmunt",
@@ -244,52 +155,30 @@ export const cardsData = [
   },
   {
     id: 16,
-    titles: "Maspujols",
+    titles: ["Maspujols", "Gratallops"],
+    portadaImg: "/images/cardsBlackAndWhite/cat-bn-16.png",
     image: "/images/cards/card-16.png",
     description:
       "Terresfertilsiramats, boscos,firesimercats sonrecursosdestacats d'unsocspriilegiats.",
     passwordImg: "19384756",
     passwordDescription: "47291038",
   },
-  {
-    id: 16,
-    titles: "Gratallops",
-    image: "/images/cards/card-16.png",
-    description:
-      "Terresfertilsiramats, boscos,firesimercats sonrecursosdestacats d'unsocspriilegiats.",
-    passwordImg: "19384756",
-    passwordDescription: "47291038",
-  },
+
   {
     id: 17,
-    titles: "La Morera de Montsant",
+    titles: ["La Morera de Montsant", "Torroja del Priorat"],
+    portadaImg: "/images/cardsBlackAndWhite/cat-bn-17.png",
     image: "/images/cards/card-17.png",
     description:
       "Pero no totrutllabe desde lomalanyprimer ; convindraempenyorar la RibailAleixar.",
     passwordImg: "91028375",
     passwordDescription: "28473956",
   },
-  {
-    id: 17,
-    titles: "Torroja del Priorat",
-    image: "/images/cards/card-17.png",
-    description:
-      "Pero no totrutllabe desde lomalanyprimer ; convindraempenyorar la RibailAleixar.",
-    passwordImg: "91028375",
-    passwordDescription: "28473956",
-  },
+
   {
     id: 18,
-    titles: "Escaladei",
-    image: "/images/cards/card-18.png",
-    description:
-      "No hihatrampesnienganys, us ho dic de veritat: delnaixement delcomtat, se'ncelebrenset-cents anys",
-    passwordImg: "19283745",
-    passwordDescription: "38475620",
-  },
-  {
-    id: 18,
-    titles: "La Vilella Alta",
+    titles: ["Escaladei", "La Vilella Alta"],
+    portadaImg: "/images/cardsBlackAndWhite/cat-bn-18.png",
     image: "/images/cards/card-18.png",
     description:
       "No hihatrampesnienganys, us ho dic de veritat: delnaixement delcomtat, se'ncelebrenset-cents anys",
@@ -298,16 +187,8 @@ export const cardsData = [
   },
   {
     id: 19,
-    titles: "La Vilella Baixa",
-    image: "/images/cards/card-19.png",
-    description:
-      "Siuspreguntenperladata no dubteuicomjo faig contesteu:el6demaig demiltres-centsvint-i-quatre",
-    passwordImg: "91029384",
-    passwordDescription: "47563829",
-  },
-  {
-    id: 19,
-    titles: "Porrera",
+    titles: ["La Vilella Baixa", "Porrera"],
+    portadaImg: "/images/cardsBlackAndWhite/cat-bn-19.png",
     image: "/images/cards/card-19.png",
     description:
       "Siuspreguntenperladata no dubteuicomjo faig contesteu:el6demaig demiltres-centsvint-i-quatre",
@@ -316,16 +197,8 @@ export const cardsData = [
   },
   {
     id: 20,
-    titles: "La Figuera",
-    image: "/images/cards/card-20.png",
-    description:
-      "I de tota aquesta historia perguardar-nelamemoria se'nfaunampliredactat ben signatisegellat.",
-    passwordImg: "10293847",
-    passwordDescription: "56473829",
-  },
-  {
-    id: 20,
-    titles: "Poboleda",
+    titles: ["La Figuera", "Poboleda"],
+    portadaImg: "/images/cardsBlackAndWhite/cat-bn-20-png",
     image: "/images/cards/card-20.png",
     description:
       "I de tota aquesta historia perguardar-nelamemoria se'nfaunampliredactat ben signatisegellat.",
